e916b39546f118ae87912b3ebf5d4317ade5d3c5
Jonathan Selander <jonathan@mondaycreative.se>
2014-04-10 21:37:28 +0200

    ok mediumtext

--- src/code/Queue/sql/queue_setup/mysql4-install-1.0.0.php
+++ src/code/Queue/sql/queue_setup/mysql4-install-1.0.0.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * MEDIUMTEXT for the handler because the messages can be around 1MB in size
+ *
+ * @author jonathan@madepeople.se
+ */
+
 $this->startSetup();
 
 $this->run("
@@ -7,7 +13,7 @@ DROP TABLE IF EXISTS {$this->getTable('made_queue_job')};
 CREATE TABLE {$this->getTable('made_queue_job')} (
     job_id INT UNSIGNED NOT NULL PRIMARY KEY auto_increment,
     queue VARCHAR(255) NOT NULL DEFAULT 'default',
-    handler TEXT,
+    handler MEDIUMTEXT,
     error TEXT,
     failed_attempts SMALLINT UNSIGNED NOT NULL DEFAULT 0,
     failed_at DATETIME NULL,
