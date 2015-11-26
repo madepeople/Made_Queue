483d26e6827fc0bc85c4bfa14282cf646f5cfe89
Jonathan Selander <selander.jonathan@gmail.com>
2015-11-26 23:55:22 +0100

    Actually do not throw an exception if dir cannot be locked

--- src/code/Queue/Model/Lock/Directory.php
+++ src/code/Queue/Model/Lock/Directory.php
@@ -26,7 +26,7 @@ class Made_Queue_Model_Lock_Directory
             $varienIo = new Varien_Io_File;
             $result = $varienIo->mkdir($baseDir, 0777, true);
             if ($result === false) {
-                throw new Exception("Couldn't create basedir for locking: $baseDir");
+                return $result;
             }
         }
 
