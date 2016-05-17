be470f3be26a93b160c2336431b2debd9e539ebf
Daniel Holmstrom <holmstrom.daniel@gmail.com>
2016-05-17 13:46:15 +0200

    Changed Queue messagebroker implementation

--- src/code/Queue/MessageBroker/Queue.php
+++ src/code/Queue/MessageBroker/Queue.php
@@ -5,7 +5,7 @@
  * This messagebroker will queue the messages in the database but not deliver them
  *
  */
-class Made_Queue_MessageBroker_Queue implements Mage_Queue_MessageBroker_Interface
+class Made_Queue_MessageBroker_Queue extends Made_Queue_MessageBroker_Abstract implements Made_Queue_MessageBroker_Interface
 {
     
     public function publishMessage(Made_Queue_Model_Message $message)
