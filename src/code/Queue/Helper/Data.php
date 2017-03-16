9304e411005b31d466aba1023036d327cc35c452
Jonathan Selander <selander.jonathan@gmail.com>
2017-03-16 10:30:01 +0100

    getFrontendMessageBroker in the helper

--- src/code/Queue/Helper/Data.php
+++ src/code/Queue/Helper/Data.php
@@ -24,4 +24,15 @@ class Made_Queue_Helper_Data extends Mage_Core_Helper_Abstract
         return $broker;
     }
 
+    final public function getFrontendMessageBroker()
+    {
+        $broker = new Made_Queue_MessageBroker_Frontend();
+
+        foreach ($this->getConfig()->getSubscribers() as $subscriber) {
+            $broker->addSubscriber($subscriber);
+        }
+
+        return $broker;
+    }
+
 }
